47a268a89304a3721dae601ea3d3ef8d8de7a073
Greg Newman <contactgregnewman@gmail.com>
2021-11-05 17:36:24 -0400

    Removed redundant routes. Added default module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,6 @@ import { Routes, RouterModule, Router } from '@angular/router';
 
 import { OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
 
-import { HomeComponent } from './components/home/home.component';
-import { AboutThisSiteComponent } from './components/about-this-site/about-this-site.component';
-import { LoginComponent } from './components/login/login.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
 export function OnAuthRequired(oktaAuth, injector) {
   const router = injector.get(Router);
 
@@ -15,16 +10,12 @@ export function OnAuthRequired(oktaAuth, injector) {
 }
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', loadChildren: () => import('./modules/default/default.module').then(m => m.DefaultModule)},
   {path: 'implicit/callback', component: OktaCallbackComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'about_this_site', component: AboutThisSiteComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'fora', loadChildren: () => import('./modules/fora/fora.module').then(m => m.ForaModule),
     canActivate: [OktaAuthGuard],
     data: { OnAuthRequired },
   },
-  {path: 'not_here', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/not_here', pathMatch: 'full'}
 ];
 
